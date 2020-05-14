2870bd9e0e644c0d2b4e3376ced93d5bf5a877b5
snnik <snnik@live.com>
2020-05-14 15:58:48 +0300

    0.5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-#Jenkins_test
 # Jenkins_test
+
+Test jobs for deploy with jenkins and ansible
--- deploy.groovy
+++ deploy.groovy
@@ -1,6 +1,9 @@
 node{
     stage "Greatings!"
-    println "Hello World"
-    home = tool name: "mc", type: "mc"
-    println home
+        println "Hello World"
+    stage "mc deploy"
+        ansiblePlaybook tst.yml
+    stage "mc home path"
+        home = tool name: "mc", type: "mc"
+        println home
 }
