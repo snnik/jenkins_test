a484c303e7d148745e49d851169b384875dc621f
snnik <snnik@live.com>
2020-05-18 17:10:24 +0300

    0.2 -1

--- deploy.groovy
+++
@@ -1,9 +0,0 @@
-node{
-    stage "Greatings!"
-        println "Hello World"
-    stage "mc deploy"
-    // ansiblePlaybook colorized: true, installation: 'main', playbook: 'tst.yml'
-    stage "mc home path"
-        home = tool name: "python3"
-        println home
-}
